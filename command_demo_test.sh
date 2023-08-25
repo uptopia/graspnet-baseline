a3af8f7e4c534c600663b2898789311f9d4ebc46
uptopia <charlotte07221@gmail.com>
2023-08-25 10:01:55 +0800

    demo code

---
+++ command_demo_test.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0 python3 demo.py --checkpoint_path /home/iclab/work/graspnet-baseline/logs/log_rs/checkpoint-rs.tar
--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 """ Demo to show prediction results.
     Author: chenxi-wang
 """
-
+# https://blog.csdn.net/zhuanzhuxuexi/article/details/132059773
 import os
 import sys
 import numpy as np
@@ -13,6 +13,10 @@ from PIL import Image
 
 import torch
 from graspnetAPI import GraspGroup
+
+# import pyrealsense2 as rs
+import cv2
+import matplotlib.pyplot as plt
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(ROOT_DIR, 'models'))
@@ -53,9 +57,29 @@ def get_and_process_data(data_dir):
     color = np.array(Image.open(os.path.join(data_dir, 'color.png')), dtype=np.float32) / 255.0
     depth = np.array(Image.open(os.path.join(data_dir, 'depth.png')))
     workspace_mask = np.array(Image.open(os.path.join(data_dir, 'workspace_mask.png')))
-    meta = scio.loadmat("/home/iclab/work/graspnet-baseline/doc/motor_data/meta.mat") #(os.path.join(data_dir, 'meta.mat'))
+    meta = scio.loadmat("/home/iclab/work/graspnet-baseline/doc/example_data/meta.mat") #(os.path.join(data_dir, 'meta.mat'))
     intrinsic = meta['intrinsic_matrix']
     factor_depth = meta['factor_depth']
+
+    # print('meta:\n', meta)
+    # print('intrinsic:\n', intrinsic)
+    # print('factor_depth:\n', factor_depth)
+    # print('workspace_mask:\n', workspace_mask) # True, False
+
+    ##====example_data====##
+    # intrinsic:
+    # [[631.54864502   0.         638.43517329]
+    # [  0.         631.20751953 366.49904066]
+    # [  0.           0.           1.        ]]
+    # factor_depth: [[1000.]]
+
+    ##====motor_data====##
+    #intrinsic: 
+    # [[919.16595459   0.         641.86254883]
+    #  [  0.         919.43945312 366.82495117]
+    #  [  0.           0.           1.        ]]
+    # factor_depth: [[1000]]
+
 
     # generate cloud
     camera = CameraInfo(1280.0, 720.0, intrinsic[0][0], intrinsic[1][1], intrinsic[0][2], intrinsic[1][2], factor_depth)
@@ -119,7 +143,63 @@ def demo(data_dir):
         gg = collision_detection(gg, np.array(cloud.points))
     vis_grasps(gg, cloud)
 
+# def cam_input():
+#     # create a pipline
+#     pipeline = rs.pipeline()
+
+#     # create a config obj to configure the pipeline
+#     config = rs.config()
+#     config.enable_stream(rs.stream.depth, 1024, 768, rs.format.z16, 30)
+#     config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+
+#     # start the pipeline
+#     pipeline.start(config)
+#     align = rs.align(rs.stream.color) #align obj for depth-color
+
+#     try:
+#         while True:
+#             #wait for coherent pair of frams: color & depth
+#             frames = pipeline.wait_for_frames()
+#             aligned_frames = align.process(frames)
+#             if not aligned_frames:
+#                 continue
+
+#             color_frame = aligned_frames.get_color_frame()
+#             aligned_depth_frame =  aligned_frames.get_depth_frame()
+
+#             if not color_frame or not aligned_depth_frame:
+#                 continue
+
+#             #convert frame to np arrays img
+#             color_frame_img = np.asanyarray(color_frame.get_data())
+#             aligned_depth_img = np.asanyarray(aligned_depth_frame.get_data())
+            
+#             # display 
+#             aligned_depth_colormap = cv2.applyColorMap(cv2.converScaleAbs(aligned_depth_img, alpha=0.03), cv2.COLORMAP_JET)
+#             cv2.imshow("Align Depth colormap", aligned_depth_colormap)
+#             cv2.imshow("aligned depth image", aligned_depth_img)
+#             cv2.imwrite('/home/iclab/work/graspnet-baseline/doc/stream_rs/depth.png', aligned_depth_img)
+
+#             cv2.imshow("aligned color image", color_frame_img)
+#             cv2.imwrite('/home/iclab/work/graspnet-baseline/doc/stream_rs/color.png', color_frame_img)
+
+#             if cv2.waitKey(1) & 0xFF == ord('q'):
+#                 break
+#     finally:
+#         pipeline.stop()
+#         cv2.destroyAllWindows()
+
 if __name__=='__main__':
-    data_dir = 'doc/example_data'
-    # data_dir = 'doc/motor_data/test6' #1~6
+    
+    # #======Method1=======#
+    # # Use Streamed Images
+    # #====================#
+    # cam_input()
+    # data_dir = 'doc/stream_rs'
+    # demo(data_dir)
+
+    #======Method2=======#
+    # Use Saved Images
+    #====================#
+    data_dir = 'doc/example_data' #'doc/motor_data/test6' #1~6
     demo(data_dir)
