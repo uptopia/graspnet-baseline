fdfe9c6a2be8e34bdc2b11fd3bf25d7320ef1deb
uptopia <charlotte07221@gmail.com>
2024-05-06 01:51:39 +0800

    add train/test_dataset to command_xx_train.sh

--- command_rvl_train.sh
+++ command_rvl_train.sh
@@ -1,5 +1,8 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 python3 train_rvl.py \
-    --camera realsense --log_dir logs/log_rs \
+    --camera realsense \
+    --log_dir logs/log_rs \
+    --train_dataset train_small \
+    --test_dataset test_small \
     --batch_size 2 \
     --dataset_root /workspace/graspnet-baseline/data/Benchmark/graspnet \
     --graspnet_lazy_mode True
