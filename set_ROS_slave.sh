0dcef8caff3dec96037fa42402baf432309b54cf
“pokePeng” <ken.man987654852@gmail.com>
2024-06-17 08:49:19 +0800

    set_ROS_slave.sh input ip from terminal

--- set_ROS_slave.sh
+++ set_ROS_slave.sh
@@ -3,6 +3,12 @@
 # echo "export ROS_MASTER_URI=http://163.13.164.152:11311" >> ~/.bashrc
 # echo "export ROS_IP=163.13.164.1" >> ~/.bashrc
 
-echo "export ROS_MASTER_URI=http://172.25.0.2:11311" >> ~/.bashrc
-echo "export ROS_IP=172.25.0.3" >> ~/.bashrc
+master_ip=$1
+ros_ip=$2
+
+echo "master_ip: ${master_ip}"
+echo "ros_ip:    ${ros_ip}"
+
+echo "export ROS_MASTER_URI=http://${master_ip}:11311" >> ~/.bashrc
+echo "export ROS_IP=${ros_ip}" >> ~/.bashrc
 source ~/.bashrc
